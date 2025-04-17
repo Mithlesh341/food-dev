2a03eee1ab2b1ee6a1957c1060eb34a3a88a5f86
Mithlesh341 <mithleshmourya2004@gmail.com>
2025-04-17 15:11:45 +0530

    Commiting

--- backend/config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://mithleshmourya2004:0mAYoEMEnDbrtbq8@cluster0.one78.mongodb.net/food-del').then(() => console.log("DB Connected"));
-}
