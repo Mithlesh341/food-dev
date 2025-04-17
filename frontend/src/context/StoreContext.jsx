7e633d5f729c78ab8b70e163212b35beffc542c9
Mithlesh341 <154442066+Mithlesh341@users.noreply.github.com>
2025-04-17 16:08:13 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -4,13 +4,13 @@ import { createContext, useEffect } from "react";
 import { useState } from "react";
 
 export const StoreContext = createContext(null)
-const url = 'http://localhost:4000'
+//const url = 'https://food-dev-backend-0ksn.onrender.com'
 
 
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-    const url = "http://localhost:4000"
+    const url = "https://food-dev-backend-0ksn.onrender.com"
     const [token, setToken] = useState("");
     const [food_list, setFoodList] = useState([]);
 
